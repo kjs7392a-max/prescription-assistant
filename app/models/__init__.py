32b849a91594de072a5a9d6ba05f8b51ad2762b7
kjs7392a-max <kjs7392a@gmail.com>
2026-04-23 20:07:41 +0900

    feat: register PatientLabHistory and PrescriptionFeedback in models __init__

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,13 @@
 from app.models.patient import PatientProfile
 from app.models.drug import DrugKnowledgeBase
 from app.models.prescription import PrescriptionLog
+from app.models.lab_history import PatientLabHistory
+from app.models.feedback import PrescriptionFeedback
 
-__all__ = ["PatientProfile", "DrugKnowledgeBase", "PrescriptionLog"]
+__all__ = [
+    "PatientProfile",
+    "DrugKnowledgeBase",
+    "PrescriptionLog",
+    "PatientLabHistory",
+    "PrescriptionFeedback",
+]
